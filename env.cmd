697d32417c0ebaa8e1d7796cd366e8967625cfd3
Fadhil Mandaga <firodj@gmail.com>
2016-09-15 22:42:37 +0700

    change order precedence tools

--- env.cmd
+++ env.cmd
@@ -1,5 +1,5 @@
 set DYNAMORIO_HOME=C:\DynamoRIO-Windows-6.0.0-6
-set PATH=%PATH%;C:\Program Files (x86)\CMake\bin
-set PATH=%PATH%;C:\Python27;C:\Python27\Scripts
-set PATH=%PATH%;%LOCALAPPDATA%\Programs\Git\bin
-set PATH=%PATH%;%DYNAMORIO_HOME%\lib32\debug
+set PATH=C:\Program Files (x86)\CMake\bin;%PATH%
+set PATH=C:\Python27;C:\Python27\Scripts;%PATH%
+set PATH=%LOCALAPPDATA%\Programs\Git\bin;%PATH%
+set PATH=%DYNAMORIO_HOME%\lib32\debug;%PATH%
