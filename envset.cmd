31a5b1497708c497a755b87fe1de48fbae2f7527
Fadhil Mandaga <firodj@gmail.com>
2016-09-15 23:11:47 +0700

    remove googltest from env

--- envset.cmd
+++ envset.cmd
@@ -3,4 +3,3 @@ set PATH=C:\Program Files (x86)\CMake\bin;%PATH%
 set PATH=C:\Python27;C:\Python27\Scripts;%PATH%
 set PATH=%LOCALAPPDATA%\Programs\Git\bin;%PATH%
 set PATH=%DYNAMORIO_HOME%\lib32\debug;%PATH%
-set GTEST_ROOT=C:\Program Files (x86)\googletest-distribution
